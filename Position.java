109017eaf578dd1925e92cdd0978ec0ddc038b65
darkobijuan <darksithsoldier@yahoo.com>
2019-09-16 16:58:48 -0700

    Add files via upload

--- Position.java
+++ Position.java
@@ -3,6 +3,8 @@ public class Position {
 	//Private variables
 	private int x;
 	private int y;
+	private int from_x; //The points we came from
+	private int from_y;
 
 	//Constructor
 	Position(){
@@ -17,6 +19,13 @@ public class Position {
 	public int getY(){
 		return y;
 	}
+	public int getFrom_X(){
+		return from_x;
+	}
+	public int getFrom_Y(){
+		return from_y;
+	}
+
 
 	//Set Methods
 	public int setX(int a){
@@ -26,5 +35,13 @@ public class Position {
 	public int setY(int b){
 		y = b;
 		return y;
+	}
+	public int setFrom_X(int a){
+		from_x = a;
+		return from_x;
+	}
+	public int setFrom_Y(int b){
+		from_y = b;
+		return from_y;
 	}
 }
